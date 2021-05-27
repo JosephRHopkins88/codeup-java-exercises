10d9a094f04633bf5ac7a58444339db79c4ae6b3
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-05-27 10:54:50 -0500

    update

---
+++ src/StringExercise.java
@@ -0,0 +1,18 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String education = "We don't need no education\nWe don't need no thought control";
+        System.out.println(education);
+        System.out.println("///////");
+        String quotations = "Check \"this\" out!, \"s inside of \"s";
+        System.out.println(quotations);
+        System.out.println("//////");
+        String driveString = "In windows, the main drive is usually C:\\";
+        System.out.println(driveString);
+        System.out.println("//////////");
+        String escapingCharacters = "I can do backslashes , double backslashes  and triple backslashes .";
+        System.out.println(escapingCharacters);
+
+
+    }
+}
---
+++ src/bob.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hey, my name's Bob... My mom said you wanted to talk to me, or something... Whatever...");
+        String response = "";
+        while(!response.equalsIgnoreCase("QUIT")) {
+            System.out.println("What do you want?");
+            response = sc.nextLine();
+            if (response.isEmpty()) {
+                System.out.println("Fine. Be that way!");
+            } else if (response.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (response.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (response.equalsIgnoreCase("quit")){
+                System.out.println("Finally...");
+            } else {
+                System.out.println("Whatever.");
+            }
+        }
+        System.out.println("I'm out of here...");
+    }
+}
